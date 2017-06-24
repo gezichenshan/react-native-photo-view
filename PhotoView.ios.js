15b670b82bff621fa95a7691234c26bd880062a4
gezichenshan <ryan@miniyun.cn>
2017-06-24 14:53:47 +0800

    Update PhotoView.ios.js
    Add a loading state in the viewer by default.

--- PhotoView.ios.js
+++ PhotoView.ios.js
@@ -3,26 +3,37 @@ import {
     View,
     ScrollView,
     Image,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,TouchableOpacity,
+    ActivityIndicator
 } from 'react-native';
 
 export default class PhotoView extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loaded: false
+        }
+    }
     render() {
         return (
             <ScrollView
                 contentContainerStyle={{ alignItems:'center', justifyContent:'center' }}
                 centerContent={true}
                 maximumZoomScale={this.props.maximumZoomScale}
-                minimumZoomScale={this.props.minimumZoomScale} 
-                showsHorizontalScrollIndicator={this.props.showsHorizontalScrollIndicator} 
-                showsVerticalScrollIndicator={this.props.showsVerticalScrollIndicator} 
-                >
+                minimumZoomScale={this.props.minimumZoomScale}>
 
                 <TouchableWithoutFeedback
                     onPress={this.props.onTap ? this.props.onTap : function() {}}>
-
-                    <Image {...this.props}/>
-
+                    {!this.state.loaded?
+                        <TouchableOpacity {...this.props}>
+                            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                                <ActivityIndicator/>
+                            </View>
+                            <Image onLoadEnd={()=>{this.setState({loaded:true})}} source={this.props.source} style={{height:1,width:1}}/>
+                        </TouchableOpacity>
+                        :
+                        <Image {...this.props}/>
+                    }
                 </TouchableWithoutFeedback>
 
             </ScrollView>
